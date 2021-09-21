93ac831d32efe3bda8a1bbd30a010090e85e23b6
Ruheej1 <ruhj707@gmail.com>
2021-09-21 18:04:06 +0530

    Updates

---
+++ precache-manifest.a90af79f27f38c3b8d997667b04f8fb2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2350f533bfd4b0a5d5f249c8a7c7fd",
+    "url": "/portfoliorj/index.html"
+  },
+  {
+    "revision": "25396324f75f19bc2e5d",
+    "url": "/portfoliorj/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8b0180286c4a91fe0639",
+    "url": "/portfoliorj/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "0896cb572530cca25dd2",
+    "url": "/portfoliorj/static/css/main.9e79e964.chunk.css"
+  },
+  {
+    "revision": "4348f8dd02312dadd221",
+    "url": "/portfoliorj/static/js/2.00862669.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfoliorj/static/js/2.00862669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25396324f75f19bc2e5d",
+    "url": "/portfoliorj/static/js/3.cc331e79.chunk.js"
+  },
+  {
+    "revision": "8b0180286c4a91fe0639",
+    "url": "/portfoliorj/static/js/4.d3915d0c.chunk.js"
+  },
+  {
+    "revision": "0896cb572530cca25dd2",
+    "url": "/portfoliorj/static/js/main.56ec79ae.chunk.js"
+  },
+  {
+    "revision": "5dca17f46c95aa19a635",
+    "url": "/portfoliorj/static/js/runtime-main.bd3d3476.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfoliorj/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfoliorj/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8072365fcf23318c77b7db80a5d5a54c",
+    "url": "/portfoliorj/static/media/bvcoe.8072365f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfoliorj/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfoliorj/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfoliorj/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfoliorj/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfoliorj/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
